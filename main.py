dd1891d472e83fe4f8f3ba8dd6b2e56fdebbd92d
leaves <leaves@JiachengdeMacBook-Pro-2.local>
2022-10-04 17:12:01 +0800

    add

--- main.py
+++ main.py
@@ -19,25 +19,33 @@ def write2file(file_name, content):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    # 打开文件
     skip_word = open("Data/skip_word").read().split("\n")
     important_word = open("Data/important_word").read().split("\n")
     data = open("Data/knowledge").read().split("****")
+
+    # 遍历所有thm
     for thm in data:
         lines = thm.split("\n")
         if len(lines) < 2:
             continue
-        print(lines)
+
+        # 遍历每个单词，筛选关键词
         keyword = []
         for word in lines[0].split():
+
+            # 预处理单个单词
             if word == "":
                 continue
-            word = word.replace(",","").replace(".", "")
-            word = word.lower()
+            word = word.replace(",", "").replace(".", "").lower()
+
+            # 单词分类
             if word in skip_word:
                 continue
             elif word in important_word:
                 keyword.append(word)
             else:
+                # 人工判断
                 while 1:
                     print("'{}' is important?".format(word))
                     ans = input()
@@ -47,6 +55,7 @@ if __name__ == '__main__':
                     elif ans == "n":
                         skip_word.append(word)
                         break
+        intro = lines[0]
         origin = lines[1]
 
     write2file("Data/skip_word", skip_word)
